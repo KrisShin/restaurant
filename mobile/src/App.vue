fc51cea245f873de015efcc57bff3defdcb3d2ad
Kris_Shin <krisshin5014@outlook.com>
2021-01-27 10:53:41 +0800

    local session

--- mobile/src/App.vue
+++ mobile/src/App.vue
@@ -12,5 +12,16 @@ import Home from "./views/Home.vue";
 export default {
   name: "App",
   component: [Home],
+  created() {
+    if (sessionStorage.getItem("store")) {
+      this.$store.replaceState(
+        Object.assign({}, JSON.parse(sessionStorage.getItem("store")))
+      );
+    }
+
+    window.addEventListener("beforeunload", () => {
+      sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+    });
+  },
 };
 </script>
