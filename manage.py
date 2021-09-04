6122c511396032e328e4fe915a12f90b95e09e5b
Kris_Shin <KrisShin5014@outlook.com>
2021-09-04 23:20:32 +0800

    [prog] Refine migrate.

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
+from flask_migrate import Migrate
+
 from config.manage_app import create_app
+from config.global_params import db
+from server_src.models import *
 
 manage = create_app()
+migrate = Migrate(manage, db)
 
 if __name__ == '__main__':
     '''Default launch script.'''
